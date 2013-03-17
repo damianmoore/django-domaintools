13072c24400feb0a3ac9eec3a7e0e6555e37073d
Damian Moore <damian@epixstudios.co.uk>
2013-03-17 14:20:17 +0000

    Added setup.py

---
+++ domaintools/setup.py
@@ -0,0 +1,14 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "django-domaintools",
+    version = "0.0.1",
+    author = "Damian Moore",
+    author_email = "django-domaintools@epixstudios.co.uk",
+    description = ("Some tools for dealing with domain names"),
+    license = "BSD",
+    keywords = "django domain",
+    url = "https://github.com/damianmoore/django-domaintools",
+    packages=['django-domaintools'],
+)
